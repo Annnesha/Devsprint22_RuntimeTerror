85d9f2e105290adf49f7b3fedacd658afb650f44
Annnesha <92147048+Annnesha@users.noreply.github.com>
2022-11-05 07:24:43 +0000

    Second Commit

--- Controllers/authControllers.go
+++ Controllers/authControllers.go
@@ -15,4 +15,142 @@ import(
   "strings"
 )
 
-const SecretKey
+const SecretKey = os.Getenv("SecretKey")
+
+func Register(c *fiber.Ctx) error {
+  var data map[string]string
+
+  if err := c.BodyParser(&data); err != nil {
+    return 
+  }
+
+  password, _ := bcrypt.GenerateFromPassword([]byte(data["password"]), 14)
+
+	if (data["email"][0] > 0 && email[len(email)-11:len(email)]=="@kiit.ac.in"){
+		user := models.Student{
+			First_name: data["first_name"],
+			Last_name : data["last_name"],
+			Email : data["email"],
+			Password: password,
+		}
+
+		database.DB.Create(&user)
+		return c.JSON(user)
+
+	} else if (email[len(email)-11:len(email)]=="@kiit.ac.in") {
+		user := models.Teacher{
+			First_name: data["first_name"],
+			Last_name:  data["last_name"],
+			Email:      data["email"],
+			Password:   password,
+		}
+
+		database.DB.Create(&user)
+		return c.JSON(user)
+	}
+
+	else{
+		c.Status(fiber.StatusNotFound)
+		return c.JSON(fiber.Map{
+			"message" : "Invalid credentials"
+		})
+	}
+}
+
+func Login(c *fiber.Ctx) error {
+	var data map[string]string
+  
+	if err := c.BodyParser(&data); err != nil {
+		return err
+	}
+  
+  if (data["email"][0] > 0 && strings.Contains((data["email"], "@kiit.ac.in"))){
+    
+	rows, err := database.DB.Query("SELECT password FROM Student WHERE email = ?", data["email"]).First(&user)
+    if err!=nil{
+      return err
+    }
+    
+  }
+
+
+
+	if user.Id == 0 {
+		c.Status(fiber.StatusNotFound)
+		return c.JSON(fiber.Map{
+			"message": "user not found",
+		})
+	}
+
+	if err := bcrypt.CompareHashAndPassword(user.Password, []byte(data["password"])); err != nil {
+		c.Status(fiber.StatusBadRequest)
+		return c.JSON(fiber.Map{
+			"message": "Wrong password",
+		})
+	}
+
+	claims := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.StandardClaims{
+		ExpiresAt: time.Now().Add(time.Hour * 24).Unix(),
+		Issuer:    strconv.Itoa(int(user.Id)),
+	})
+
+	token, err := claims.SignedString([]byte(SecretKey))
+
+	if err != nil {
+		c.Status(fiber.StatusInternalServerError)
+		return c.JSON(fiber.Map{
+			"message": "could not login",
+		})
+	}
+
+	cookie := fiber.Cookie{
+		Name:     "jwt",
+		Value:    token,
+		Expires:  time.Now().Add(time.Hour * 24),
+		HTTPOnly: true,
+	}
+
+	c.Cookie(&cookie)
+
+	return c.JSON(fiber.Map{
+		"message": "success",
+	})
+}
+
+func User(c *fiber.Ctx) error {
+	cookie := c.Cookies("jwt")
+
+	token, err := jwt.ParseWithClaims(cookie, &jwt.StandardClaims{}, func(token *jwt.Token) (interface{}, error) {
+		return []byte(SecretKey), nil
+	})
+
+	if err != nil {
+		c.Status(fiber.StatusUnauthorized)
+		return c.JSON(fiber.Map{
+			"message": "Unauthorized",
+		})
+	}
+
+	claims := token.Claims.(*jwt.StandardClaims)
+
+	var user models.User
+
+	database.DB.Where("id = ?", claims.Issuer).First(&user)
+
+	return c.JSON(user)
+}
+
+func Logout(c *fiber.Ctx) error {
+	cookie := fiber.Cookie{
+		Name:     "jwt",
+		Value:    "",
+		Expires:  time.Now().Add(-time.Hour),
+		HTTPOnly: true,
+	}
+
+	c.Cookie(&cookie)
+
+	return c.JSON(fiber.Map{
+		"message": "success",
+	})
+}
